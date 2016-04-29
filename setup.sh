ec94f97f63617b53bc7fecb5b44083b6ba8bb7b7
Pridexs <alehstk@gmail.com>
2016-04-29 15:27:28 -0400

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx
-
+sudo pip3 install virtualenv
 virtualenv -p python3 pridexsenv
 pridexsenv/bin/pip3 install -r requirements.txt
