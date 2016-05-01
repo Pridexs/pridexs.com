cb5010f77056efa85115bcad2f4469bc0df18903
Pridexs <alehstk@gmail.com>
2016-05-01 14:33:10 -0400

    Fixed base dir

--- pridexs/pridexs/settings/base.py
+++ pridexs/pridexs/settings/base.py
@@ -14,7 +14,7 @@ import os
 from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 SECRET_KEY = 'SOME_SECRET_KEY'
 
